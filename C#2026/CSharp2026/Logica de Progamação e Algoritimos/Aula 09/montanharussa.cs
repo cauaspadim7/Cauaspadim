d7437bc66c052714d020f64d5843fb80edf775cb
cauaspadim7 <cauaspadim7@gmail.com>
2026-03-02 21:17:09 -0300

    Aula 09

--- C#2026/CSharp2026/Logica de Progamação e Algoritimos/Aula 09/montanharussa.cs
+++ C#2026/CSharp2026/Logica de Progamação e Algoritimos/Aula 09/montanharussa.cs
@@ -1,31 +1,25 @@
 using System;
 
-class Program
+Console.Write("Digite o número de alturas das pessoas: ");
+int numeroPessoas = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura mínima (cm): ");
+int alturaMinima = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura máxima (cm): ");
+int alturaMaxima = int.Parse(Console.ReadLine());
+
+int contador = 0;
+
+for (int i = 1; i <= numeroPessoas; i++)
 {
-    static void Main()
+    Console.Write($"Digite a altura da pessoa número {i}: ");
+    int altura = int.Parse(Console.ReadLine());
+
+    if (altura >= alturaMinima && altura <= alturaMaxima)
     {
-        Console.Write("Digite o número de alturas das pessoas: ");
-        int numeroPessoas = int.Parse(Console.ReadLine());
-
-        Console.Write("Digite a altura mínima (cm): ");
-        int alturaMinima = int.Parse(Console.ReadLine());
-
-        Console.Write("Digite a altura máxima (cm): ");
-        int alturaMaxima = int.Parse(Console.ReadLine());
-
-        int contador = 0;
-
-        for (int i = 1; i <= numeroPessoas; i++)
-        {
-            Console.Write($"Digite a altura da pessoa número {i}: ");
-            int altura = int.Parse(Console.ReadLine());
-
-            if (altura >= alturaMinima && altura <= alturaMaxima)
-            {
-                contador++;
-            }
-        }
-
-        Console.WriteLine($"{contador} pessoas que estão entre {alturaMinima} e {alturaMaxima} cm de altura.");
+        contador++;
     }
 }
+
+Console.WriteLine($"{contador} pessoas que estão entre {alturaMinima} e {alturaMaxima} cm de altura.");
