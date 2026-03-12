bd80860e79983d32bbd24b2f334643e16d01b995
cauaspadim7 <cauaspadim7@gmail.com>
2026-03-11 21:50:58 -0300

    Aula 12

---
+++ C#2026/CSharp2026/Logica de Progamação e Algoritimos/Aula 12/tupla.cs
@@ -0,0 +1,13 @@
+//Tupla
+using static System.Console;
+var clodoaldo = (34, 34, "Heitor", "SENAI", 'B');
+
+WriteLine($"idade do clodoaldo: {clodoaldo.Item1}");
+WriteLine($"idade do clodoaldo: {clodoaldo.Item2}");
+WriteLine($"Nome do filho do clodo: {clodoaldo.Item3}");
+clodoaldo.Item1 =54;
+WriteLine($"idade do clodoaldo: {clodoaldo.Item1}");
+
+(int, int , int) numeros = (1 , 2 ,5);
+WriteLine($"Numeros: {numeros.Item1}, {numeros.Item2}, {numeros.Item3}");
+
