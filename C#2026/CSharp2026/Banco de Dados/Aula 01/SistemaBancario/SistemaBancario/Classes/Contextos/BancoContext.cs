536fdfb23864d8a814b50b21e53b445adc38b038
cauaspadim7 <cauaspadim7@gmail.com>
2026-04-22 21:46:27 -0300

    Aula 02

--- C#2026/CSharp2026/Banco de Dados/Aula 01/SistemaBancario/SistemaBancario/Classes/Contextos/BancoContext.cs
+++ C#2026/CSharp2026/Banco de Dados/Aula 01/SistemaBancario/SistemaBancario/Classes/Contextos/BancoContext.cs
@@ -11,29 +11,31 @@ namespace SistemaBancario.Classes.Contextos
         /// Representa a tabela de contas bancarias no banco de dados
         /// DbSet permite realizar operaçoes CRUD 
         /// </summary>
-        public DbSet <Banco> Contas { get; set; }
+        public DbSet<Banco> Contas { get; set; }
 
 
         //Metodos 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite(@"Data Source = BancoDB.db");
+            optionsBuilder.UseSqlServer(
+          @"Server=ECFP507D1319382\SQLEXPRESS;Database=BancoDB;Trusted_Connection=True;TrustServerCertificate=True;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           modelBuilder.Entity<Banco>(entity=>
-           {
-               entity.HasKey(e =>e.Id);
-               entity.Property(e => e.NumeroConta).IsRequired();
-               entity.Property(e => e.Titular).IsRequired().HasMaxLength(50);
-               entity.Property(e => e.Saldo).HasColumnType("Decimal(18,2)");
-           }
-               
-               );
+            modelBuilder.Entity<Banco>(
+                entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.NumeroConta).IsRequired();
+                entity.Property(e => e.Titular).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Saldo).HasColumnType("Decimal(18,2)");
+            }
+
+                );
         }
 
     }
-}
+    }
 
 
 
---
+++ C#2026/CSharp2026/Banco de Dados/Aula 02/Estudante/Estudante/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Estudante.Classes.Entidades;
+using Estudante.Classes.Dados;
+
+using var context = new AlunoContext();
+
+context.Database.EnsureCreated();
+
+Aluno pessoa1 = new Aluno("Caua", 12345, "Phython");
+context.Alunos.Add(pessoa1);
+context.SaveChanges();
+
